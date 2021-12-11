75a980ed68f9eefbd9d375eb036d363c208b544b
APilganchuk <73530904+APilganchuk@users.noreply.github.com>
2021-12-11 22:56:14 +0000

    Deploying to gh-pages from @ APilganchuk/parcel-project-test-task@9ce7cb3eaa41f19a4a719481de5fe1618f3d86f1 🚀

--- src.868b0f4a.js
+++ src.868b0f4a.js
@@ -20,4 +20,4 @@ var e,t=arguments[3];!function(t,r){"object"==typeof exports&&"object"==typeof m
 },{"./get-refs":"EGsz","lodash.debounce":"PZFh","../templates/books-list.hbs":"hfDW","./fetchBooks":"UIuz"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/pop-up"),require("./js/main");
 },{"./sass/main.scss":"clu1","./js/pop-up":"NRD9","./js/main":"d6sW"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.0ec381ce.js.map
+//# sourceMappingURL=/parcel-project-test-task/src.868b0f4a.js.map
